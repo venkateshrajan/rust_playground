6255f002892f0b4c0e078c55b1713207b2d16d78
venkateshrajan <venkatesh.xaviers@gmail.com>
2024-10-05 20:02:04 +0530

    An Ok Stack

--- toomanylists/lists/src/lib.rs
+++ toomanylists/lists/src/lib.rs
@@ -1,12 +1,2 @@
 pub mod first;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::mem::size_of;
-
-    #[test]
-    fn it_works() {
-        println!("{}", size_of::<first::List>());
-    }
-}
+pub mod second;
